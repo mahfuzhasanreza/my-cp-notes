52c1cce8177f16137553376512aa55eff5a88e5f
Mahfuz Hasan Reza <115473925+mahfuzhasanreza@users.noreply.github.com>
2024-01-26 01:50:57 +0600

    Update and rename temp.cpp to myTemplate.cpp

---
+++ myTemplate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fori(i, n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vl vector<ll>
+
+void solve() {
+    
+}
+
+int main() {
+    ll t=1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
