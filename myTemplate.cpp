deb60311fa36241799aa488ca084102a9b8a2891
Mahfuz Hasan Reza <115473925+mahfuzhasanreza@users.noreply.github.com>
2024-01-27 17:28:30 +0600

    Update myTemplate.cpp

--- myTemplate.cpp
+++ myTemplate.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 #define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define nl endl
 #define yes cout << "YES" << endl;
 #define no cout << "NO" << endl;
 #define fori(i, n) for(int i=0; i<n; i++)
-#define vi vector<int>
-#define vl vector<ll>
+#define presum if(i==0) psum[i]=arr[i]; else psum[i]=psum[i-1]+arr[i];
 
 void solve() {
     
